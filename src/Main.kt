d74284f9481bf5bf6a007a08bf6dae4fb85fd7b6
dantekadagi3 <135029772+dantekadagi3@users.noreply.github.com>
2024-03-23 04:47:06 -0700

    variable declaration

---
+++ src/Main.kt
@@ -0,0 +1,9 @@
+
+
+//Here we are going to learn about variable declaration in Kotlin
+//Note that in kotlin it is not compulsory to specify the data type as it can
+//automatically be determined
+fun main() {
+    var age:Int=20
+
+}
